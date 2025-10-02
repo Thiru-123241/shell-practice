2387e23c8693eaa6bd5e239d0313e4125aa12cfa
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-02 17:37:49 +0530

    added variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Thiru
+PERSON2=suresh
+
+echo "$PERSON1 : Hi bro whats up"
+echo "$PERSON2 : Hi $PERSON1 i am fine too and u"
+echo "$PERSON1 : I am also fine what is going on"
+echo "$PERSON2 : Nothing is going on
