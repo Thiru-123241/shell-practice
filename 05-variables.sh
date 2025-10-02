213076449af5399318690ce43545ff1f84602713
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-02 22:58:57 +0530

    Added another variable

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hi bro whats up"
+echo "$PERSON2 : Hi $PERSON1 i am fine too and u"
+echo "$PERSON1 : I am also fine what is going on"
+echo "$PERSON2 : Nothing is going on"
