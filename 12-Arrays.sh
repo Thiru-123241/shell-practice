78483d24f9f1e0516fc5e9858f9f7b1b17baf96d
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-03 22:28:12 +0530

    added sum value

---
+++ 12-Arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Movies=("oG" "court" "Hit" "kgf")
+echo "First movie : ${Movies[0]}"
+echo "Third movie : ${Movies[3]}"
+echo "All movies : ${Movies[@]}"
+echo "second movie : $Movies[2]"
+echo "Fourth movie : ${Movies[4]}"
+echo "Test : $Movies"
