4d224d47e23d79957f449f55edc5efe6e937f9e0
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-03 22:19:33 +0530

    added sum value

--- Arrays.sh
+++ Arrays.sh
@@ -2,3 +2,5 @@ Movies=("pushpa" "court" "Hit" "kgf")
 echo "First movie : ${Movies[0]}"
 echo "Third movie : ${Movies[3]}"
 echo "All movies : ${Movies[@]}"
+echo "second movie : $Movies[2]"
+echo "Fourth movie : ${Movies[4]}"
